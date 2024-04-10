a6fdee2f5aa7eff4a14845d30a57825ad1ee723e
Nicholas D. Socci <soccin@mskcc.org>
2024-04-10 18:26:59 -0400

    Updates for filter01

--- filter01.R
+++ filter01.R
@@ -1,5 +1,13 @@
 x11 = function (...) grDevices::x11(...,type='cairo')
 
+format_maf<-function(maf) {
+    maf %>%
+        select(1,5,6,9,11,13,16,HGVSp_Short,t_vaf,t_alt_count,n_vaf,n_depth,CALLERS,FILTERS) %>%
+        mutate(Tumor_Sample_Barcode=fct_reorder(Tumor_Sample_Barcode,as.numeric(gsub(".*RR","",Tumor_Sample_Barcode)))) %>%
+        arrange(Tumor_Sample_Barcode,desc(t_vaf),Start_Position)
+}
+
+
 require(tidyverse)
 
 callerOrder=c("mutect2", "freebayes", "strelka")
@@ -16,7 +24,7 @@ ps=min(QUAL[QUAL>0],na.rm=T)
 mm=mm %>%
     mutate(t_vaf=t_alt_count/t_depth,n_vaf=n_alt_count/n_depth) %>%
     mutate(FILTER=case_when(
-                    CALLER=="freebayes" & QUAL>1 & t_vaf>5*n_vaf & t_alt_count>=8 ~ "PASS",
+                    CALLER=="freebayes" & QUAL>15000 & t_vaf>5*n_vaf ~ "PASS",
                     T ~ FILTER
                 )
     ) %>%
@@ -37,27 +45,43 @@ m1=mm %>%
 #     distinct(ETAG,Tumor_Sample_Barcode,.keep_all=T) %>%
 #     filter(!is.na(HGVSp_Short) & !grepl("=$",HGVSp_Short))
 
+T_VAF_CUT=0.05
+
+maf1=m1 %>%
+    filter(N.PASS>=1) %>%
+    filter(t_vaf >= T_VAF_CUT & t_alt_count >= 8 & t_depth >= 20 & t_vaf > 5*n_vaf) %>%
+    distinct(ETAG,Tumor_Sample_Barcode,.keep_all=T) %>%
+    filter(!is.na(HGVSp_Short) & !grepl("=$",HGVSp_Short))
+
+tblCust=mafCust %>% format_maf %>% filter(Hugo_Symbol %in% c("Rb1","Pten"))
+tbl1=maf1 %>% format_maf %>% filter(Hugo_Symbol %in% c("Rb1","Pten"))
+
+openxlsx::write.xlsx(
+    list(HighSens=tbl1,CUSTOM=tblCust),
+    "p15529_MusVar_V1_FilterCustom_2024-03-24.xlsx"
+)
+
 mafHC=m1 %>%
     filter(N.PASS>=2) %>%
-    filter(t_vaf >= 0.05 & t_alt_count >= 8 & t_depth >= 20 & t_vaf > 5*n_vaf) %>%
+    filter(t_vaf >= T_VAF_CUT & t_alt_count >= 8 & t_depth >= 20 & t_vaf > 5*n_vaf) %>%
     distinct(ETAG,Tumor_Sample_Barcode,.keep_all=T) %>%
     filter(!is.na(HGVSp_Short) & !grepl("=$",HGVSp_Short))
 
 maf10=m1 %>%
     filter(N.PASS>=1 & N.CALL>=2) %>%
-    filter(t_vaf >= 0.05 & t_alt_count >= 8 & t_depth >= 20 & t_vaf > 5*n_vaf) %>%
+    filter(t_vaf >= T_VAF_CUT & t_alt_count >= 8 & t_depth >= 20 & t_vaf > 5*n_vaf) %>%
     distinct(ETAG,Tumor_Sample_Barcode,.keep_all=T) %>%
     filter(!is.na(HGVSp_Short) & !grepl("=$",HGVSp_Short))
-
-maf1=m1 %>%
-    filter(N.PASS>=1) %>%
-    filter(t_vaf >= 0.05 & t_alt_count >= 8 & t_depth >= 20 & t_vaf > 5*n_vaf) %>%
-    distinct(ETAG,Tumor_Sample_Barcode,.keep_all=T) %>%
-    filter(!is.na(HGVSp_Short) & !grepl("=$",HGVSp_Short))
-
 
 maf2=m1 %>%
     filter(N.CALL>=2) %>%
-    filter(t_vaf >= 0.05 & t_alt_count >= 8 & t_depth >= 20 & t_vaf > 5*n_vaf) %>%
+    filter(t_vaf >= T_VAF_CUT & t_alt_count >= 8 & t_depth >= 20 & t_vaf > 5*n_vaf) %>%
     distinct(ETAG,Tumor_Sample_Barcode,.keep_all=T) %>%
     filter(!is.na(HGVSp_Short) & !grepl("=$",HGVSp_Short))
+
+maf12=m1 %>%
+    filter(N.CALL>=2 | N.PASS>=1) %>%
+    filter(t_vaf >= T_VAF_CUT & t_alt_count >= 8 & t_depth >= 20 & t_vaf > 5*n_vaf) %>%
+    distinct(ETAG,Tumor_Sample_Barcode,.keep_all=T) %>%
+    filter(!is.na(HGVSp_Short) & !grepl("=$",HGVSp_Short))
+
