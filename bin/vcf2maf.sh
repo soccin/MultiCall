06e7eeb5bc79f94c5a56a366020d1ce189dfceae
Nicholas D. Socci <soccin@mskcc.org>
2024-04-10 19:58:36 -0400

    Fix paths. Still need install stuff

--- bin/vcf2maf.sh
+++ bin/vcf2maf.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+module load samtools/1.19.2
+
 SDIR="$( cd "$( dirname "$0" )" && pwd )"
 
 MROOT=$SDIR/../resources/
 VEPROOT=$MROOT/vep
+VEPDATA=
+
 VCF2MAF=$MROOT/vcf2maf-1.6.21/vcf2maf.pl
 
 IVCF=$1
@@ -12,9 +16,9 @@ TID=$3
 
 perl $VCF2MAF \
     --vep-path $VEPROOT/bin \
-    --vep-data $VEPROOT/vep_data \
+    --vep-data $VEPDATA \
     --vep-forks 12  \
-    --ref-fasta $VEPROOT/vep_data/mus_musculus/102_GRCm38/Mus_musculus.GRCm38.dna.toplevel.fa.gz \
+    --ref-fasta $VEPDATA/mus_musculus/102_GRCm38/Mus_musculus.GRCm38.dna.toplevel.fa.gz \
     --species mus_musculus \
     --ncbi-build GRCm38 \
     --retain-info CALLER,DP \
