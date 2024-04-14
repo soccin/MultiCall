ef7c8da87aa8cd2b52ed78509528af2cab5114d7
Nicholas D. Socci <soccin@mskcc.org>
2024-04-14 17:45:09 -0400

    Changes to include vardict

--- filter01.R
+++ filter01.R
@@ -10,7 +10,7 @@ format_maf<-function(maf) {
 
 require(tidyverse)
 
-callerOrder=c("mutect2", "freebayes", "strelka")
+callerOrder=c("mutect2", "freebayes", "strelka", "vardict")
 
 mm=fs::dir_ls("int",rec=T,regex=".rda") %>% map(readRDS) %>% bind_rows %>% type_convert
 
@@ -19,7 +19,6 @@ QUAL=as.numeric(ifelse(QUAL==".",-1,QUAL))
 QUAL[QUAL<0]=NA
 mm$QUAL=QUAL
 ps=min(QUAL[QUAL>0],na.rm=T)
-
 
 mm=mm %>%
     mutate(t_vaf=t_alt_count/t_depth,n_vaf=n_alt_count/n_depth) %>%
@@ -31,35 +30,32 @@ mm=mm %>%
     mutate(CALLER=factor(CALLER,levels=callerOrder)) %>%
     arrange(ETAG,CALLER,Tumor_Sample_Barcode)
 
+val1Cols=colnames(mm)[setdiff(which(apply(mm,2,\(x)len(unique(x)))==1),1:20)]
+
 m1=mm %>%
+    select(-any_of(val1Cols)) %>%
+    mutate(STATUS=ifelse(is.na(STATUS),"",STATUS)) %>%
     group_by(ETAG,Tumor_Sample_Barcode) %>%
     mutate(
         N.CALL=n(),N.PASS=sum(FILTER=="PASS"),
         CALLERS=paste(CALLER,collapse=";"),
-        FILTERS=paste(FILTER,collapse="|")
+        FILTERS=paste(FILTER,collapse="|"),
+        STATUS=paste(STATUS,collapse="|")
         ) %>%
     ungroup
 
-# maf0=m1 %>%
-#     filter(t_vaf >= 0.05 & t_alt_count >= 8 & t_depth >= 20 & t_vaf > 5*n_vaf) %>%
-#     distinct(ETAG,Tumor_Sample_Barcode,.keep_all=T) %>%
-#     filter(!is.na(HGVSp_Short) & !grepl("=$",HGVSp_Short))
-
 T_VAF_CUT=0.05
+
+maf0=m1 %>%
+    filter(t_vaf >= 0.05 & t_alt_count >= 8 & t_depth >= 20 & t_vaf > 5*n_vaf) %>%
+    distinct(ETAG,Tumor_Sample_Barcode,.keep_all=T) %>%
+    filter(!is.na(HGVSp_Short) & !grepl("=$",HGVSp_Short))
 
 maf1=m1 %>%
     filter(N.PASS>=1) %>%
     filter(t_vaf >= T_VAF_CUT & t_alt_count >= 8 & t_depth >= 20 & t_vaf > 5*n_vaf) %>%
     distinct(ETAG,Tumor_Sample_Barcode,.keep_all=T) %>%
     filter(!is.na(HGVSp_Short) & !grepl("=$",HGVSp_Short))
-
-tblCust=mafCust %>% format_maf %>% filter(Hugo_Symbol %in% c("Rb1","Pten"))
-tbl1=maf1 %>% format_maf %>% filter(Hugo_Symbol %in% c("Rb1","Pten"))
-
-openxlsx::write.xlsx(
-    list(HighSens=tbl1,CUSTOM=tblCust),
-    "p15529_MusVar_V1_FilterCustom_2024-03-24.xlsx"
-)
 
 mafHC=m1 %>%
     filter(N.PASS>=2) %>%
