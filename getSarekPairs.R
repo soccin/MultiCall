735cab4f0db68222641fbb3a2ae3d5de11136307
Nicholas D. Socci <soccin@mskcc.org>
2024-04-15 13:23:37 -0400

    Add code to create bam pair args

--- getSarekPairs.R
+++ getSarekPairs.R
@@ -3,8 +3,36 @@ suppressPackageStartupMessages({
 })
 
 argv=commandArgs(trailing=T)
-manifest=read_csv(argv,progress=F,show_col_types = FALSE)
-normals=manifest %>% filter(status==0) %>% select(patient,normal=sample)
-tumors=manifest %>% filter(status==1) %>% select(patient,tumor=sample)
-pairs=full_join(normals,tumors,by="patient") %>% select(patient,normal,tumor)
-cat(format_tsv(pairs,col_names=F))
+
+if(len(argv)==1) {
+
+    manifest=read_csv(argv[1],progress=F,show_col_types = FALSE) %>% select(patient,sample,status)
+    normals=manifest %>% filter(status==0) %>% select(patient,normal=sample)
+    tumors=manifest %>% filter(status==1) %>% select(patient,tumor=sample)
+    pairs=full_join(normals,tumors,by="patient") %>% select(patient,normal,tumor)
+    cat(format_tsv(pairs,col_names=F))
+
+} else if(len(argv)==2) {
+
+    manifest=read_csv(argv[1],progress=F,show_col_types = FALSE) %>% select(patient,sample,status)
+
+    bams=fs::dir_ls(argv[2],recur=T,regex=".recal.cram$")
+    bams=tibble(sample=gsub(".recal.(cram)","",basename(bams)),bam=bams)
+
+    manifest=left_join(manifest,bams,by="sample")
+
+    normals=manifest %>% filter(status==0) %>% select(patient,normal=sample,normalBam=bam)
+    tumors=manifest %>% filter(status==1) %>% select(patient,tumor=sample,tumorBam=bam)
+
+    pairs=left_join(normals,tumors,by="patient") %>% select(normalBam,tumorBam)
+
+    cat(format_tsv(pairs,col_names=F))
+
+} else {
+
+    cat("\n usage:\n")
+    cat("    getSarekPairs.R sarek_input.csv\n")
+    cat("    getSarekPairs.R sarak_input.csv path/to/bams\n\n")
+    quit()
+
+}
