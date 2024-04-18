ad653a4bb55c8442811f50cb7f5c060a461f45ea
Nicholas D. Socci <soccin@mskcc.org>
2024-04-18 17:46:58 -0400

    Bug when multiple fastq's per sample; path 1

--- getSarekPairs.R
+++ getSarekPairs.R
@@ -4,19 +4,18 @@ suppressPackageStartupMessages({
 
 argv=commandArgs(trailing=T)
 
+manifest=read_csv(argv[1],progress=F,show_col_types = FALSE) %>%
+    select(patient,sample,status) %>%
+    distinct
+
 if(len(argv)==1) {
 
-    manifest=read_csv(argv[1],progress=F,show_col_types = FALSE) %>% select(patient,sample,status)
     normals=manifest %>% filter(status==0) %>% select(patient,normal=sample)
     tumors=manifest %>% filter(status==1) %>% select(patient,tumor=sample)
     pairs=full_join(normals,tumors,by="patient") %>% select(patient,normal,tumor)
     cat(format_tsv(pairs,col_names=F))
 
 } else if(len(argv)==2) {
-
-    manifest=read_csv(argv[1],progress=F,show_col_types = FALSE) %>%
-        select(patient,sample,status) %>%
-        distinct
 
     bams=fs::dir_ls(argv[2],recur=T,regex=".recal.cram$")
     bams=tibble(sample=gsub(".recal.(cram)","",basename(bams)),bam=bams)
