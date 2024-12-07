7d585af36db3dc0a062cf7b2a6969f781e20ba4a
Nicholas D. Socci <soccin@mskcc.org>
2024-12-07 15:25:48 -0500

    Reorganize target folder/add Twist targets

---
+++ targets/M-IMPACT_v2_GRCm38/target.resources.sh
@@ -0,0 +1,4 @@
+#
+# N.B.: SDIR needs to be set by whatever script is sourcing this file
+#
+TARGET_BED=$SDIR/targets/M-IMPACT_v2_GRCm38/M-IMPACT_v2_mm10_targets_plus15bp.bed
---
+++ targets/Twist_Mouse_Exome_Target_Rev1_GRCm38/target.resources.sh
@@ -0,0 +1,4 @@
+#
+# N.B.: SDIR needs to be set by whatever script is sourcing this file
+#
+TARGET_BED=$SDIR/targets/Twist_Mouse_Exome_Target_Rev1_GRCm38/Twist_Mouse_Exome_Target_Rev1_GRCm38_targets_plus15bp.bed
